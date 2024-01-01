5dcb8979fff7d47b22e6990c3b9c294f68aea106
Yayat <muhammadhidayatullahaspar@gmail.com>
2024-01-01 15:29:27 +0800

    factorial based on user input

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("input number for factorial");
+		int number = int.Parse(Console.ReadLine());
+		long factorial = Factorial(number);
+		Console.WriteLine($"the result is {factorial}");
+	}
+	static long Factorial(int x)
+	{
+		if (x <= 1)
+		{
+			return 1;
+		}
+		else 
+		{
+			return x*Factorial(x-1);
+		}
+	}
+}
